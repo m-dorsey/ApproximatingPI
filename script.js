86f82b179154d3dded9a9bad2cae7de58244525e
m-dorsey <dorseymakenzie@gmail.com>
2022-01-22 16:53:27 -0500

    setup functions needed for implementation

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 
+// scene manager
+var manager;
+
 function setup() {
+
+    manager = new SceneManager();
+    manager.addScene(StartScreen);
+    manager.addScene(ApproxScreen);
+
+    manager.showNextScene();
+
+    createCanvas(600, 600);
 
 }
 
 function draw() {
-    
+
+    manager.draw();
+
+}
+
+function StartScreen() {
+
+    this.enter = function() {
+
+    }
+
+    this.draw = function() {
+
+        background("#e6e6e6");
+
+    }
+
+}
+
+function ApproxScreen() {
+
+    this.enter = function() {
+
+    }
+
+    this.draw = function() {
+
+        background("#333");
+
+    }
+
 }
