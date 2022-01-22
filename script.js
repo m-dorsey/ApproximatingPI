0206e796da8624392cd0c8e9ef7347fdfe9be54a
m-dorsey <dorseymakenzie@gmail.com>
2022-01-22 17:49:49 -0500

    added text fields to show pi approx in progress

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var manager;
 // canvas lengths
 var canvasHeight, canvasWidth;
 
+// offset
+var offset;
+
 function setup() {
 
     manager = new SceneManager();
@@ -13,8 +16,9 @@ function setup() {
 
     manager.showScene(ApproxScreen);
 
-    canvasHeight = canvasWidth = 600;
-    createCanvas(canvasHeight, canvasWidth);
+    canvasHeight = canvasWidth = 800;
+    offset = 100;
+    createCanvas(canvasWidth-offset, canvasHeight);
 
 }
 
@@ -41,6 +45,9 @@ function StartScreen() {
 function ApproxScreen() {
 
     var randPtsBtn;
+    var piApprox = 0.0;
+    var totalPts = 0;
+    var ptsInCircle = 0;
 
     this.enter = function() {
 
@@ -56,24 +63,54 @@ function ApproxScreen() {
 
         this.drawCenterCircle();
 
+        textSize(16);
+        textAlign(LEFT);
+        text(
+            "PI Approximation: " + piApprox,
+            10, 20
+        );
+        text(
+            "Number of Points Inside: " + ptsInCircle,
+            10, 50
+        );
+        text(
+            "Total Number of Points: " + totalPts,
+            10, 80
+        );
+
+        text("(" + int(mouseX) + ", " + int(mouseY) + ")", int(mouseX), int(mouseY));
+
+
+
 
 
     }
 
     this.drawCenterCircle = function() {
 
+
         stroke("#000");
-        strokeWeight(1);
+        strokeWeight(0.5);
+
+        // container square
+        fill("#e6e6e6");
+        rect(0, offset, canvasWidth-offset, canvasHeight-offset);
+
+        // center circle
         fill("#fff");
-        circle(canvasWidth/2, canvasHeight/2, canvasWidth-50);
+        circle((canvasWidth/2)-(offset/2), (canvasHeight/2)+(offset/2), canvasWidth-offset);
+
+        // center point
         fill("#000");
-        circle(canvasWidth/2, canvasHeight/2, 5);
+        circle((canvasWidth/2)-(offset/2), (canvasHeight/2)+(offset/2), 3);
+
+
 
     }
 
     this.initBtns = function() {
 
-        randPtsBtn.locate(10, 10);
+        randPtsBtn.locate(canvasWidth-260, 10);
         randPtsBtn.resize(150, 30);
         randPtsBtn.color = "#fff";
         randPtsBtn.cornerRadius = 3;
@@ -81,7 +118,7 @@ function ApproxScreen() {
         randPtsBtn.text = "Generate Random Points";
         randPtsBtn.textSize = 12;
         randPtsBtn.onPress = function() {
-
+            generateRandPts();
         }
         randPtsBtn.onHover = function() {
             randPtsBtn.stroke = "#333";
@@ -94,6 +131,10 @@ function ApproxScreen() {
             randPtsBtn.textColor = "#000";
         }
 
+    }
+
+    this.generateRandPts = function() {
+        
     }
 
 }
