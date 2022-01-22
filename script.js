08430826d83556eb26c7366eaeb1b39e6e983cef
m-dorsey <dorseymakenzie@gmail.com>
2022-01-22 17:19:19 -0500

    created a button to generate random points

--- script.js
+++ script.js
@@ -40,13 +40,19 @@ function StartScreen() {
 
 function ApproxScreen() {
 
+    var randPtsBtn;
+
     this.enter = function() {
+
+        randPtsBtn = new Clickable();
+        this.initBtns();
 
     }
 
     this.draw = function() {
 
         background("#e6e6e6");
+        randPtsBtn.draw();
 
         this.drawCenterCircle();
 
@@ -56,10 +62,37 @@ function ApproxScreen() {
 
     this.drawCenterCircle = function() {
 
+        stroke("#000");
+        strokeWeight(1);
         fill("#fff");
         circle(canvasWidth/2, canvasHeight/2, canvasWidth-50);
         fill("#000");
         circle(canvasWidth/2, canvasHeight/2, 5);
+
+    }
+
+    this.initBtns = function() {
+
+        randPtsBtn.locate(10, 10);
+        randPtsBtn.resize(150, 30);
+        randPtsBtn.color = "#fff";
+        randPtsBtn.cornerRadius = 3;
+        randPtsBtn.strokeWeight = 1;
+        randPtsBtn.text = "Generate Random Points";
+        randPtsBtn.textSize = 12;
+        randPtsBtn.onPress = function() {
+
+        }
+        randPtsBtn.onHover = function() {
+            randPtsBtn.stroke = "#333";
+            randPtsBtn.color = "#bbb";
+            randPtsBtn.textColor = "#fff";
+        }
+        randPtsBtn.onOutside = function() {
+            randPtsBtn.stroke = "#000";
+            randPtsBtn.color = "#fff";
+            randPtsBtn.textColor = "#000";
+        }
 
     }
 
